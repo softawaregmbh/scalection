1cd108308df64a5490f3a1791f9b71830b8c5b18
Felix Eberhard <felix.eberhard@softaware.at>
2024-05-27 11:23:22 +0200

    Add sql server dependency and update nuget packages

--- src/Scalection/Scalection.AppHost/Program.cs
+++ src/Scalection/Scalection.AppHost/Program.cs
@@ -2,6 +2,10 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var apiService = builder.AddProject<Projects.Scalection_ApiService>("apiservice");
 
+var sqlServer = builder.AddSqlServer("sqlserver")
+                       .PublishAsAzureSqlDatabase()
+                       .AddDatabase("sqldb");
+
 builder.AddProject<Projects.Scalection_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(apiService);
